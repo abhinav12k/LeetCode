0931186d4d63a074f62193454dfcd2dfff008b39
Abhinav <abhi2000.rtk@gmail.com>
2021-05-10 20:16:44 +0530

    Trapping water bug fix

--- Stack/TrappingWater.java
+++ Stack/TrappingWater.java
@@ -56,7 +56,7 @@ public class TrappingWater {
 
             left_max[0] = height[0];
             for(int i=1;i<size;i++)
-                left_max[1]=Math.max(height[i],left_max[i-1]);
+                left_max[i]=Math.max(height[i],left_max[i-1]);
 
             right_max[size-1] = height[size-1];
             for(int i=size-2;i>=0;i--)
