78e22e4de02eb86ae09c276a63f366d04601b666
Abhinav <abhi2000.rtk@gmail.com>
2021-10-06 10:33:29 +0530

    added optimizations

--- DynamicProgramming/PalindromePartitioning.java
+++ DynamicProgramming/PalindromePartitioning.java
@@ -11,6 +11,7 @@ public class PalindromePartitioning {
         for (int[] row : t)
             Arrays.fill(row, -1);
         System.out.println(getMinPartitions(str, 0, str.length() - 1));
+        System.out.println(getMinPartitionOptimized(str, 0, str.length() - 1));
     }
 
     static int[][] t;
@@ -32,6 +33,35 @@ public class PalindromePartitioning {
         for (int k = i; k <= j - 1; k++) {
             int curr = getMinPartitions(str, i, k) + getMinPartitions(str, k + 1, j) + 1;
             ans = Math.min(ans, curr);
+        }
+        t[i][j] = ans;
+        return ans;
+    }
+
+    private static int getMinPartitionOptimized(String str,int i,int j){
+
+        if(i>j)
+            return 0;
+
+        if(isPalindrome(str,i,j))
+            return 0;
+
+        if(t[i][j]!=-1)
+            return t[i][j];
+
+        int ans = Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+            int left = 0;
+            if(t[i][k]!=-1)
+                left = t[i][k];
+            else
+                left = getMinPartitionOptimized(str,i,k);
+            int right = 0;
+            if(t[k+1][j]!=-1)
+                right =  t[k+1][j];
+            else
+                right = getMinPartitionOptimized(str,k+1,j);
+            ans = Math.min(ans, left+right+1);
         }
         t[i][j] = ans;
         return ans;
