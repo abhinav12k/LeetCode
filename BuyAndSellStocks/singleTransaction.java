314d9c2dd3d7afca6afe77fadb7e01ff79b2ebd1
Abhinav <abhi2000.rtk@gmail.com>
2021-11-05 14:53:40 +0530

    listed all formats

--- BuyAndSellStocks/singleTransaction.java
+++ BuyAndSellStocks/singleTransaction.java
@@ -1,4 +1,20 @@
 package BuyAndSellStocks;
 
 public class singleTransaction {
+
+    public static void main(String[] args) {
+
+        int[] prices = {11, 6, 7, 19, 4, 1, 6, 18, 4};
+
+        int min = prices[0];
+        int profit = 0;
+
+        for(int i=1;i<prices.length;i++){
+            min = Math.min(min,prices[i]);
+            profit = Math.max(profit,prices[i]-min);
+        }
+
+        System.out.println(profit);
+    }
+
 }
