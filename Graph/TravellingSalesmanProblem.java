6c19b81ca359b2ebe3f1a5ab3f8291b9a902ec9e
Abhinav <abhi2000.rtk@gmail.com>
2021-12-20 20:20:36 +0530

    traveling salesman using dp and bitmasking

--- Graph/TravellingSalesmanProblem.java
+++ Graph/TravellingSalesmanProblem.java
@@ -1,19 +1,24 @@
 package Graph;
 
+import java.util.Arrays;
+
 public class TravellingSalesmanProblem {
 
-    static int tcp(int[][] dist, int setOfCities, int city, int n) {
+    static int tcp(int[][] dist, int setOfCities, int city, int n, int[][] dp) {
 
         if (setOfCities == (1 << n) - 1) {
             return dist[city][0];
         }
 
+        if (dp[setOfCities][city] != -1)
+            return dp[setOfCities][city];
+
         int ans = Integer.MAX_VALUE;
         for (int choice = 0; choice < n; choice++) {
 
-            if((setOfCities & (1<<choice)) == 0){
-                int subProblem = dist[city][choice] + tcp(dist,(setOfCities | (1<<choice)),choice,n);
-                ans = Math.min(ans,subProblem);
+            if ((setOfCities & (1 << choice)) == 0) {
+                int subProblem = dist[city][choice] + tcp(dist, (setOfCities | (1 << choice)), choice, n, dp);
+                ans = Math.min(ans, subProblem);
             }
 
         }
@@ -27,7 +32,12 @@ public class TravellingSalesmanProblem {
                 {42, 30, 0, 10},
                 {25, 34, 10, 0}};
 
-        System.out.println(tcp(dist, 1, 0, dist.length));
+        int n = dist.length;
+        int[][] dp = new int[1 << n][n];
+        for (int i = 0; i < 1 << n; i++)
+            Arrays.fill(dp[i], -1);
+
+        System.out.println(tcp(dist, 1, 0, n, dp));
 
     }
 
